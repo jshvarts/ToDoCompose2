4e78ff54e1eb1e723e8a91502a21e11ceb0b6ceb
James Shvarts <jshvarts@gmail.com>
2021-11-28 16:18:22 -0500

    Populate task detail screen dynamically

--- app/src/main/java/com/jshvarts/todocompose/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/jshvarts/todocompose/ui/screen/task/TaskScreen.kt
@@ -2,14 +2,22 @@ package com.jshvarts.todocompose.ui.screen.task
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import com.jshvarts.todocompose.data.models.Priority
 import com.jshvarts.todocompose.data.models.ToDoTask
+import com.jshvarts.todocompose.ui.viewmodel.SharedViewModel
 import com.jshvarts.todocompose.util.Action
 
 @Composable
 fun TaskScreen(
     selectedTask: ToDoTask?,
+    sharedViewModel: SharedViewModel,
     navigateToListScreen: (Action) -> Unit
 ) {
+    val title: String by sharedViewModel.title
+    val description: String by sharedViewModel.description
+    val priority: Priority by sharedViewModel.priority
+
     Scaffold(
         topBar = {
             TaskAppBar(
@@ -17,6 +25,21 @@ fun TaskScreen(
                 navigateToListScreen = navigateToListScreen
             )
         },
-        content = {}
+        content = {
+            TaskContent(
+                title = title,
+                onTitleChange = {
+                    sharedViewModel.title.value = it
+                },
+                description = description,
+                onDescriptionChange = {
+                    sharedViewModel.description.value = it
+                },
+                priority = priority,
+                onPrioritySelected = {
+                    sharedViewModel.priority.value = it
+                }
+            )
+        }
     )
 }
