c59683dbf51db12ff76311dc04b12934ce3f13d7
James Shvarts <jshvarts@gmail.com>
2021-11-28 16:18:21 -0500

    Add ViewModel with StateFlow

--- app/src/main/java/com/jshvarts/todocompose/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/jshvarts/todocompose/data/repository/ToDoRepository.kt
@@ -2,9 +2,11 @@ package com.jshvarts.todocompose.data.repository
 
 import com.jshvarts.todocompose.data.ToDoDao
 import com.jshvarts.todocompose.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
 
     val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
