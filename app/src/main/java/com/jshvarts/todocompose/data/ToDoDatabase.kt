0b8c04a20da8af5331c9731cc7480d9df24d7122
James Shvarts <jshvarts@gmail.com>
2021-11-28 16:18:21 -0500

    Create database and dao

---
+++ app/src/main/java/com/jshvarts/todocompose/data/ToDoDatabase.kt
@@ -0,0 +1,10 @@
+package com.jshvarts.todocompose.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.jshvarts.todocompose.data.models.ToDoTask
+
+@Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase : RoomDatabase() {
+    abstract fun toDoDao(): ToDoDao
+}
