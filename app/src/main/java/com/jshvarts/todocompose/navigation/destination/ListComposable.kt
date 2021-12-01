4576e030075042874dc53be4d3803c21297e7ff8
James Shvarts <jshvarts@gmail.com>
2021-11-30 21:07:11 -0500

    Fix snackbar showing up after configuration changes

--- app/src/main/java/com/jshvarts/todocompose/navigation/destination/ListComposable.kt
+++ app/src/main/java/com/jshvarts/todocompose/navigation/destination/ListComposable.kt
@@ -3,12 +3,17 @@ package com.jshvarts.todocompose.navigation.destination
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.jshvarts.todocompose.ui.screen.list.ListScreen
 import com.jshvarts.todocompose.ui.viewmodel.SharedViewModel
+import com.jshvarts.todocompose.util.Action
 import com.jshvarts.todocompose.util.Constants.LIST_ARG_KEY
 import com.jshvarts.todocompose.util.Constants.LIST_SCREEN
 import com.jshvarts.todocompose.util.toAction
@@ -27,8 +32,13 @@ fun NavGraphBuilder.listComposable(
     ) { navBackStackEntry ->
         val action = navBackStackEntry.arguments?.getString(LIST_ARG_KEY).toAction()
 
-        LaunchedEffect(key1 = action) {
-            sharedViewModel.action.value = action
+        var myAction by rememberSaveable { mutableStateOf(Action.NO_ACTION) }
+
+        LaunchedEffect(key1 = myAction) {
+            if (action != myAction) {
+                myAction = action
+                sharedViewModel.action.value = action
+            }
         }
 
         val databaseAction = sharedViewModel.action.value
